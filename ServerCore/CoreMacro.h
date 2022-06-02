1d1c08a6857c99c66055eb2635fecb4e0477c803
kim <bank2014@naver.com>
2022-06-02 20:36:04 +0900

    ADD: Macro operation for Locks

---
+++ ServerCore/CoreMacro.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define OUT
+
+/*---------------
+	  Lock
+---------------*/
+
+#define USE_MANY_LOCKS(count)	Lock _locks[count];
+#define USE_LOCK				USE_MANY_LOCKS(1)
+#define	READ_LOCK_IDX(idx)		ReadLockGuard readLockGuard_##idx(_locks[idx], typeid(this).name());
+#define READ_LOCK				READ_LOCK_IDX(0)
+#define	WRITE_LOCK_IDX(idx)		WriteLockGuard writeLockGuard_##idx(_locks[idx], typeid(this).name());
+#define WRITE_LOCK				WRITE_LOCK_IDX(0)
+
