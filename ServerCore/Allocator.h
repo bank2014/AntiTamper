64e8a296205a5ab470df6f571367e78eb2225ac7
kim <bank2014@naver.com>
2022-06-02 19:05:32 +0900

    ADD: STL allocator

--- ServerCore/Allocator.h
+++ ServerCore/Allocator.h
@@ -37,3 +37,32 @@ public:
 	static void		Release(void* ptr);
 };
 
+
+
+/*-------------------
+	STL Allocator
+-------------------*/
+
+template<typename T>
+class StlAllocator
+{
+public:
+	using value_type = T;
+
+	StlAllocator() { }
+
+	template<typename Other>
+	StlAllocator(const StlAllocator<Other>&) { }
+
+	T* allocate(size_t count)
+	{
+		const int32 size = static_cast<int32>(count * sizeof(T));
+		return static_cast<T*>(xalloc(size));
+	}
+
+	void deallocate(T* ptr, size_t count)
+	{
+		xrelease(ptr);
+	}
+};
+
