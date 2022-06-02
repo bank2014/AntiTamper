a914c4e1c483101658484ec42e6b9041fbff2694
kim <bank2014@naver.com>
2022-06-02 19:12:52 +0900

    ADD: Object pool operation: pop

---
+++ ServerCore/ObjectPool.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Types.h"
+#include "MemoryPool.h"
+
+template<typename Type>
+class ObjectPool
+{
+public:
+	template<typename... Args>
+	static Type* Pop(Args&&... args)
+	{
+#ifdef _STOMP
+		MemoryHeader* ptr = reinterpret_cast<MemoryHeader*>(StompAllocator::Alloc(s_allocSize));
+		Type* memory = static_cast<Type*>(MemoryHeader::AttachHeader(ptr, s_allocSize));
+#else
+		Type* memory = static_cast<Type*>(MemoryHeader::AttachHeader(s_pool.Pop(), s_allocSize));
+#endif		
+		new(memory)Type(forward<Args>(args)...); // placement new
+		return memory;
+	}
+
+	// TODO
+
+};
+
