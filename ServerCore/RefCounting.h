862fba06352da93ff4f0ec1148c2565b50e6343d
kim <bank2014@naver.com>
2022-06-02 19:26:50 +0900

    ADD: Reference counting class

---
+++ ServerCore/RefCounting.h
@@ -0,0 +1,28 @@
+#pragma once
+
+/*---------------
+   RefCountable
+----------------*/
+
+class RefCountable
+{
+public:
+	RefCountable() : _refCount(1) { }
+	virtual ~RefCountable() { }
+
+	int32 GetRefCount() { return _refCount; }
+
+	int32 AddRef() { return ++_refCount; }
+	int32 ReleaseRef()
+	{
+		int32 refCount = --_refCount;
+		if (refCount == 0)
+		{
+			delete this;
+		}
+		return refCount;
+	}
+
+protected:
+	atomic<int32> _refCount;
+};
