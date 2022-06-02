fbc235bb2cab3cb88ca5e7b34dcba0be378a21b3
kim <bank2014@naver.com>
2022-06-02 20:57:08 +0900

    ADD: DeadlockManager - PushLock

--- ServerCore/DeadLockProfiler.cpp
+++ ServerCore/DeadLockProfiler.cpp
@@ -5,19 +5,52 @@
 	DeadLockProfiler
 ---------------------*/
 
-void DeadLockProfiler::PushLock()
+void DeadLockProfiler::PushLock(const char* name)
 {
+	LockGuard guard(_lock);
 
+	// 아이디를 찾거나 발급한다.
+	int32 lockId = 0;
+
+	auto findIt = _nameToId.find(name);
+	if (findIt == _nameToId.end())
+	{
+		lockId = static_cast<int32>(_nameToId.size());
+		_nameToId[name] = lockId;
+		_idToName[lockId] = name;
+	}
+	else
+	{
+		lockId = findIt->second;
+	}
+
+	// 잡고 있는 락이 있었다면
+	if (_lockStack.empty() == false)
+	{
+		// 기존에 발견되지 않은 케이스라면 데드락 여부 다시 확인한다.
+		const int32 prevId = _lockStack.top();
+		if (lockId != prevId)
+		{
+			set<int32>& history = _lockHistory[prevId];
+			if (history.find(lockId) == history.end())
+			{
+				history.insert(lockId);
+				CheckCycle();
+			}
+		}
+	}
+
+	_lockStack.push(lockId);
 }
 
 void DeadLockProfiler::PopLock()
 {
-
+	//TODO
 }
 
 void DeadLockProfiler::CheckCycle()
 {
-
+	//TODO
 }
 
 void DeadLockProfiler::Dfs()
--- ServerCore/DeadLockProfiler.h
+++ ServerCore/DeadLockProfiler.h
@@ -11,7 +11,7 @@ class DeadLockProfiler
 {
 public:
 	// TODO
-	void PushLock();
+	void PushLock(const char* name);
 	void PopLock();
 	void CheckCycle();
 
@@ -19,6 +19,18 @@ private:
 	void Dfs();
 
 private:
-	// TODO
+	unordered_map<const char*, int32>	_nameToId;
+	unordered_map<int32, const char*>	_idToName;
+	stack<int32>						_lockStack;
+	map<int32, set<int32>>				_lockHistory;
+
+	Mutex _lock;
+
+private:
+	vector<int32>	_discoveredOrder; // 노드가 발견된 순서를 기록하는 배열
+	int32			_discoveredCount = 0; // 노드가 발견된 순서
+	vector<bool>	_finished; // Dfs(i)가 종료 되었는지 여부
+	vector<int32>	_parent;
+
 };
 
