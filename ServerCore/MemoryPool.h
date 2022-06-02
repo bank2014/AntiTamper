6ff872e6a3c770e15b8baeb01530d5b91623405e
kim <bank2014@naver.com>
2022-06-02 18:34:37 +0900

    ADD: Memory header

--- ServerCore/MemoryPool.h
+++ ServerCore/MemoryPool.h
@@ -12,7 +12,22 @@ enum
 DECLSPEC_ALIGN(SLIST_ALIGNMENT)
 struct MemoryHeader : public SLIST_ENTRY
 {
-	// TODO 
+	// [MemoryHeader][Data]
+	MemoryHeader(int32 size) : allocSize(size) { }
+
+	static void* AttachHeader(MemoryHeader* header, int32 size)
+	{
+		new(header)MemoryHeader(size); // placement new
+		return reinterpret_cast<void*>(++header);
+	}
+
+	static MemoryHeader* DetachHeader(void* ptr)
+	{
+		MemoryHeader* header = reinterpret_cast<MemoryHeader*>(ptr) - 1;
+		return header;
+	}
+
+	int32 allocSize;
 };
 
 /*-----------------
